cf04735af297fcce7da4a90b3dc7198919f60e05
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-01-26 07:14:26 -0600

    145278

--- TuNombre4rtoNanoGames2D/Assets/Scenes/DestroyPots22012025/Code/AvatarController.cs
+++ TuNombre4rtoNanoGames2D/Assets/Scenes/DestroyPots22012025/Code/AvatarController.cs
@@ -1,16 +1,90 @@
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class AvatarController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+namespace Gavryk.PlaneationVideoGames {
+    public class AvatarController : MonoBehaviour {
+        #region ENUM
+        enum Agent {
+            IDLE,
+            MOVE,
+            ATTACK
+        }
+        #endregion ENUM
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region Variables
+        [SerializeField] GameObject gameObject;
+        [SerializeField] GameObject targetJamJar;
+        [SerializeField] Rigidbody2D rbPlayer;
+        [SerializeField] Rigidbody2D rbJamJar;
+
+        [SerializeField] Animation animationPlayer;
+        [SerializeField] Animator animatorPlayer;
+        [SerializeField] AnimatorController animatorController;
+        [SerializeField] Agent agent;
+        [SerializeField] float speedNormalPlayer;
+        [SerializeField] float runningPlayer;
+        bool IsTouchJamJar;
+        #endregion Variables
+
+        #region UnityMethods
+        void Start() {
+            rbPlayer = GetComponent<Rigidbody2D>();
+            rbJamJar = GetComponent<Rigidbody2D>();
+            agent = GetComponent<Agent>();
+            agent = Agent.IDLE;
+        }
+
+        void Update() {
+            switch (agent) {
+                case Agent.IDLE:
+                    IDLEPlayer();
+                    break;
+                case Agent.MOVE:
+                    MovePlayer();
+                    break;
+                case Agent.ATTACK:
+
+                    break;
+            }
+        }
+        #endregion UnityMethods
+
+        #region PrivateMethods
+
+        #region MovePlayer
+        void IDLEPlayer(InputAction.CallbackContext value) {
+            //animationPlayer = ;
+        }
+        void MovePlayer(InputAction.CallbackContext value) {
+
+
+
+        }
+        void RunPlayer(InputAction.CallbackContext value) {
+
+        }
+        #endregion MovePlayer
+
+        #region AttackPlayer
+        void AttackPlayer(InputAction.CallbackContext value) {
+
+        }
+        #endregion AttackPlayer
+
+        #endregion PrivateMethods
+
+        #region PublicMethods
+        public void OnCollisionEnter2D(Collision2D collision) {
+
+        }
+        public void OnTriggerEnter(Collider other) {
+
+        }
+
+
+        #endregion PublciMethods
+
     }
 }
