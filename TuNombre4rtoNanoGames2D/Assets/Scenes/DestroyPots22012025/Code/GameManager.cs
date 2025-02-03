e6c93bb781baec6211520b352d7643a071660cef
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-02-03 17:19:30 -0600

    21458

--- TuNombre4rtoNanoGames2D/Assets/Scenes/DestroyPots22012025/Code/GameManager.cs
+++ TuNombre4rtoNanoGames2D/Assets/Scenes/DestroyPots22012025/Code/GameManager.cs
@@ -2,58 +2,47 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using Gavryk.PlaneationVideoGames;
+using UnityEngine.Assertions.Must;
 
-public class GameManager : MonoBehaviour
-{// timer y condicion de victoria al destruir todos 
+public class GameManager : MonoBehaviour {// timer y condicion de victoria al destruir todos 
 
-    //https://medium.com/@eveciana21/creating-a-stopwatch-timer-in-unity-f4dff748030d
-
+    [SerializeField, HideInInspector] bool timerActive;
     [SerializeField] float timer;
     [SerializeField] TMP_Text timeTxt;
 
-    [SerializeField, HideInInspector] bool winner;
-    [SerializeField] TMP_Text victoryTxt;
-
     [SerializeField] GameObject[] vessel;
-    //[SerializeField] TextMeshProUGUI DerrotaTxt;
-    AvatarController scriptAvatar;
 
-    private void Awake()
-    {
+    [SerializeField] GameObject vicotoryPanel;
+    [SerializeField] GameObject losePanel;
+
+    //private void Awake() {
+    //    IncrementCronometer();
+    //}
+    void Start() {
+        timerActive = true;
+        timer = 0f;
+    }
+    void Update() {
+        VictoryAndLosePanelActive();
         IncrementCronometer();
     }
-    void Start()
-    {
-        timer = 0;
-    }
-
-    void Update()
-    {
-        VictoryAndLose();
-    }
-    void IncrementCronometer()
-    {
-        timer += Time.deltaTime;
-        timeTxt.text = "Tiempo:" + " " ;
-    }
-    void VictoryAndLose()
-    {
-        if (scriptAvatar)
-        {
-            victoryTxt.gameObject.SetActive(false);
-            IncrementCronometer();
-            int objects = 59;
-            if (objects >= 0)
-            {
-                Destroy(vessel[0]);
-                scriptAvatar.gameObject.SetActive(true);
-                winner = true;
-                victoryTxt.gameObject.SetActive(true);
-                print("Ganaste");
-            }
-            // implementar la derrota, pero como en este minijuego es imposible perder porque deberas de destruir todo sin importar el tiempo limite
-            //pues no es neceario en este caso 
+    void IncrementCronometer() {
+        if (timerActive == true) {
+            timer += Time.deltaTime;
+            timeTxt.text = timer.ToString("F2");
         }
     }
+    void VictoryAndLosePanelActive() {
+        // Elimina los objetos destruidos de la lista
+        vessel = System.Array.FindAll(vessel, obj => obj != null); //lo busque en el internet
+        if (vessel.Length == 0) {
+            timerActive = false;
+            vicotoryPanel.SetActive(true); 
+            Debug.Log("¡Ganaste!");
+        }
+    }
+    // implementar la derrota, pero como en este minijuego es imposible perder porque deberas de destruir todo sin importar el tiempo limite
+    //pues no es neceario en este caso 
 }
+
 
